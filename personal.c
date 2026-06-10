ede02f55190b287d6130f523fddd5543394b7848
Amanda Rosa <amandarosas2016@gmail.com>
2026-06-10 18:43:08 -0300

    Implementando cadastro de treino com series

--- personal.c
+++ personal.c
@@ -6,7 +6,34 @@ typedef struct {
     char exercicio[100];
     int peso;
     int repeticoes;
+    int series;
 } Treino;
 
 Treino treinos[100];
 int totalTreinos = 0;
+
+void cadastrarTreino() {
+    if (totalTreinos >= 100) {
+        printf("Limite de treinos atingido!\n");
+        return;
+    }
+
+    printf("Nome do aluno: ");
+    scanf(" %99[^\n]", treinos[totalTreinos].nome_aluno);
+
+    printf("Exercicio: ");
+    scanf(" %99[^\n]", treinos[totalTreinos].exercicio);
+
+    printf("Peso: ");
+    scanf("%d", &treinos[totalTreinos].peso);
+
+    printf("Repeticoes: ");
+    scanf("%d", &treinos[totalTreinos].repeticoes);
+
+    printf("Series: ");
+    scanf("%d", &treinos[totalTreinos].series);
+
+    totalTreinos++;
+
+    printf("Treino cadastrado com sucesso!\n");
+}

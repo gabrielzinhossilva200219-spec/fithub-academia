152c4628d3bd9b8db98f5212c884750320cf4d46
Daniel Lima <danielthelimma@gmail.com>
2026-06-14 16:45:16 -0300

    Implementar cadastro de alunos

--- recepcionista.c
+++ recepcionista.c
@@ -7,3 +7,30 @@ recepcionista.c
 Aluno alunos[MAX_ALUNOS];
 
 int totalAlunos = 0;
+
+void cadastrarAluno() {
+
+    printf("Nome: ");
+    scanf(" %[^\n]", alunos[totalAlunos].nome);
+
+    printf("CPF: ");
+    scanf("%s", alunos[totalAlunos].cpf);
+
+    printf("Data de nascimento: ");
+    scanf("%s", alunos[totalAlunos].dataNascimento);
+
+    printf("Email: ");
+    scanf("%s", alunos[totalAlunos].email);
+
+    printf("Telefone: ");
+    scanf("%s", alunos[totalAlunos].telefone);
+
+    printf("Laudo medico: ");
+    scanf(" %[^\n]", alunos[totalAlunos].laudo);
+
+    alunos[totalAlunos].faltas = 0;
+
+    totalAlunos++;
+
+    printf("Aluno cadastrado!\n");
+}
